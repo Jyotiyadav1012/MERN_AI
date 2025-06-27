67afae22f2567caec152bba3990ba10855df9fcb
Jyoti Yadav <jyotiyadav101202@gmail.com>
2025-06-28 01:08:47 +0530

    react app

---
+++ React/my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () =>{
+    const studentName = "John Doe";
+    const studentAge = 20;
+    const studentCity = "Reading";
+    
+return (
+    <div>
+        <h1>Student: {studentName}</h1>
+        <p>Age: <b>{age}</b></p>
+        <p>Address: <b>{studentCity}</b></p>
+    </div>
+  );
+}
+export default App;
